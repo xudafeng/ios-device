a4f84a9d234c913e9da809568cf23755049a89b4
zenzhu <zic.zhu@gmail.com>
2016-12-07 11:57:38 +0800

    Add ios-deploy to deps

---
+++ test/ios-device.test.js
@@ -0,0 +1,22 @@
+/* ================================================================
+ * ios-simulator by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Thu Feb 18 2016 21:24:24 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright  xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+'use strict';
+
+var Device = require('../lib/ios-device');
+
+describe('lib/ios-device.js', function() {
+  it('should be', function() {
+    Device.should.be.ok;
+  });
+});
